ede5ae03330e39681b3ec236b4141c94c0044c83
William Santoso <williamsantoso93@yahoo.com>
2020-11-25 10:27:05 +0700

    Initial Commit

---
+++ Unsplash Kraken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash Kraken
+//
+//  Created by William Santoso on 25/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
